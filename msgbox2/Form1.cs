de3c78ba33d7ee61e51430fd6d4ca1b29c86980f
Kyla N. Marjes <kylanmarjes@iskolarngbayan.pup.edu.ph>
2022-04-27 20:07:15 +0800

    assignment 1

---
+++ msgbox2/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace msgbox2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Kyla N. Marjes", "Message Box", MessageBoxButtons.OKCancel, MessageBoxIcon.Information)==DialogResult.OK)
+
+            { 
+             MessageBox.Show("I hope you're ok <333");
+            }
+                
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
